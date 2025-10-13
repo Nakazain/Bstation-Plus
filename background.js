348a702dd7459b67606f8b1ea68e81d66cd14786
Nakazain <naufalkaruniawanzain@gmail.com>
2025-10-13 15:19:42 +0700

    Initial init

---
+++ background.js
@@ -0,0 +1,4 @@
+document.documentElement.style.setProperty('--ext-bg', '#000000ff');
+document.documentElement.style.setProperty('--ext-text', '#f0f8ff');
+document.body.style.backgroundColor = 'var(--ext-bg)';
+document.body.style.color = 'var(--ext-text)';
